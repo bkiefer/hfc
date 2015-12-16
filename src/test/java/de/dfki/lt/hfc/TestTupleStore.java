25cc75b44365f31ff0e06e2a18ed56a84da4001a
LeraLalala <read-me@inbox.ru>
2015-12-16 17:33:42 +0300

    new test case

---
+++ src/test/java/de/dfki/lt/hfc/TestTupleStore.java
@@ -0,0 +1,14 @@
+package de.dfki.lt.hfc;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestTupleStore {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
