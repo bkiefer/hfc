d9ccd0796b7bba21ed034c9c188fac8688640991
Bernd Kiefer <bernd.kiefer@dfki.de>
2016-01-26 18:52:10 +0100

    Added Uli's "tests" for LGetLatest aggregate

--- src/test/java/de/dfki/lt/hfc/TestUtils.java
+++ src/test/java/de/dfki/lt/hfc/TestUtils.java
@@ -4,12 +4,17 @@ import java.io.File;
 import java.nio.file.Path;
 
 public class TestUtils {
+  private static final File testResourceDir = new File("src/test/data/");
   private static final File resourceDir = new File("src/resources/");
   private static final File tmpDir = new File("/tmp");
 
   public static String getResource(String name) {
     // System.out.println(new File(".").getAbsolutePath());
     return new File(resourceDir, name).getPath();
+  }
+
+  public static String getTestResource(String subdir, String name) {
+    return new File(new File(testResourceDir, subdir), name).getPath();
   }
 
   public static String getTempFile(String name) {
