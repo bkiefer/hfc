bed54698939c774432971b765e9829debbe339ff
Bernd Kiefer <bernd.kiefer@dfki.de>
2016-01-29 11:04:26 +0100

    Fixed implementation of TestUtils

--- src/test/java/de/dfki/lt/hfc/TestUtils.java
+++ src/test/java/de/dfki/lt/hfc/TestUtils.java
@@ -1,6 +1,9 @@
 package de.dfki.lt.hfc;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
 import de.dfki.lt.hfc.BindingTable.BindingTableIterator;
 
@@ -23,32 +26,41 @@ public class TestUtils {
     return new File(tmpDir, name).getPath();
   }
 
+  /** Check if expected contains all the rows that are in the BindingTable,
+   *  even if there are duplicates
+   */
   public static boolean checkResult(String[][] expected,
       BindingTable bt, String ... vars) {
-    int rows = 0;
-    //boolean result;
     try {
-      BindingTableIterator it = bt.iterator(vars);
-      while (it.hasNext()) {
-        String[] tuple = it.nextAsString();
+      // to make sure there can be multiple equal rows, we erase all rows
+      // one by one from the List, which must be empty in the end
+      List<String[]> exp = Arrays.asList(expected);
+      BindingTableIterator bindIt = bt.iterator(vars);
+      if (bindIt.hasSize() != expected.length)
+        return false; // wrong size
+
+      while (bindIt.hasNext()) {
+        String[] tuple = bindIt.nextAsString();
         // make sure all tuples from expected are covered: find the right one
-        // TODO: WRITE THE CODE AND APPLY IT CORRECTLY IN THE TESTS
-        Iterator<String[]> exp = expected.iterator();
-        while (exp.hasNext()) {
-          String[] other = exp.next();
-          if (tuple.equals(other)) {
-           //result = true; 
-           ++rows;
-           break;
-          } 
-        //return result;
+        boolean found = false;
+        for(Iterator<String[]> expIt = exp.iterator(); expIt.hasNext();) {
+          String[] expectedRow = expIt.next();
+          // This does not go into the elements of an array, will only return
+          // true if tuple == other, which is never the case
+          // if (tuple.equals(other)) {
+          if (Arrays.equals(tuple, expectedRow)) {
+            expIt.remove();
+            found = true;
+            continue; // take the next row
+          }
+        }
+        if (! found) return false;
       }
-      }
+      return exp.isEmpty();
     }
     catch (BindingTableIteratorException e) {
       throw new RuntimeException(e); // should never happen
     }
-    return rows == expected.length;
   }
 
 }
