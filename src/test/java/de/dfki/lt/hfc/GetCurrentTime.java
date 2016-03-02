3bcca4c1b25f929dcc1b4338ce2505b07f776c6b
Bernd Kiefer <bernd.kiefer@dfki.de>
2016-03-02 13:24:07 +0100

    Fix imports

--- src/test/java/de/dfki/lt/hfc/GetCurrentTime.java
+++ src/test/java/de/dfki/lt/hfc/GetCurrentTime.java
@@ -5,6 +5,8 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import de.dfki.lt.hfc.aggregates.TestLGetLatest;
+
 import static de.dfki.lt.hfc.TestUtils.checkResult;
 
 public class GetCurrentTime {
