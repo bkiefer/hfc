63ab9c290e45f4ef4da242fbdaedba8da7da4ff3
Bernd Kiefer <bernd.kiefer@dfki.de>
2016-09-02 15:58:31 +0200

    Compare floats with Float.compare

--- src/main/java/de/dfki/lt/hfc/operators/FNotEqual.java
+++ src/main/java/de/dfki/lt/hfc/operators/FNotEqual.java
@@ -6,7 +6,7 @@ import de.dfki.lt.hfc.types.XsdFloat;
 /**
  * checks whether the first argument is not equal to the second argument;
  * arguments are assumed to be numbers of type xsd:float
- * 
+ *
  * @return FunctionalOperator.TRUE or FunctionalOperator.FALSE
  *
  * @see FunctionalOperator
@@ -16,17 +16,19 @@ import de.dfki.lt.hfc.types.XsdFloat;
  * @version Wed Jun 23 15:19:56 CEST 2010
  */
 public final class FNotEqual extends FunctionalOperator {
-	
+
 	/**
 	 * note that apply() does NOT check at the moment whether the int args
 	 * represent in fact XSD floats;
 	 * note that apply() does NOT check whether it is given exactly two arguments
 	 */
 	public int apply(int[] args) {
-		if (((XsdFloat)getObject(args[0])).value != ((XsdFloat)getObject(args[0])).value)
+		if (Float.compare(
+        ((XsdFloat)getObject(args[0])).value,
+        ((XsdFloat)getObject(args[1])).value) != 0)
 			return FunctionalOperator.TRUE;
 		else
 			return FunctionalOperator.FALSE;
 	}
-	
+
 }
