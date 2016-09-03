5fa4329b299ca396db76514aeb21187f6a84b29e
Christophe Biwer <christophe.biwer@dfki.de>
2016-09-03 18:47:51 +0200

    corrected IEqual and INotEqual - do I really have to explain?

--- src/main/java/de/dfki/lt/hfc/operators/IEqual.java
+++ src/main/java/de/dfki/lt/hfc/operators/IEqual.java
@@ -6,7 +6,7 @@ import de.dfki.lt.hfc.types.XsdInt;
 /**
  * checks whether the first argument is equal to the second argument;
  * arguments are assumed to be numbers of type xsd:int
- * 
+ *
  * @return FunctionalOperator.TRUE or FunctionalOperator.FALSE
  *
  * @see FunctionalOperator
@@ -16,17 +16,17 @@ import de.dfki.lt.hfc.types.XsdInt;
  * @version Wed Jun 23 11:04:27 CEST 2010
  */
 public final class IEqual extends FunctionalOperator {
-	
+
 	/**
 	 * note that apply() does NOT check at the moment whether the int args
 	 * represent in fact XSD ints;
 	 * note that apply() does NOT check whether it is given exactly two arguments
 	 */
 	public int apply(int[] args) {
-		if (((XsdInt)getObject(args[0])).value == ((XsdInt)getObject(args[0])).value)
+		if (((XsdInt)getObject(args[0])).value == ((XsdInt)getObject(args[1])).value)
 			return FunctionalOperator.TRUE;
 		else
 			return FunctionalOperator.FALSE;
 	}
-	
+
 }
