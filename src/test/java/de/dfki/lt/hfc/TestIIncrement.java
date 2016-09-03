c95c6275d0c8e358b43cae02446bc2d00f539f2b
Christophe Biwer <christophe.biwer@dfki.de>
2016-09-03 18:49:11 +0200

    added ALL tests of Integer operations

---
+++ src/test/java/de/dfki/lt/hfc/TestIIncrement.java
@@ -0,0 +1,59 @@
+package de.dfki.lt.hfc;
+
+import static de.dfki.lt.hfc.TestUtils.*;
+import static org.junit.Assert.*;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.junit.Test;
+
+public final class TestIIncrement {
+
+  @Test
+  public void testcleanUpTuple() throws FileNotFoundException,
+      WrongFormatException, IOException {
+
+    // load Namespace
+    Namespace namespace = new Namespace(getTestResource("default.ns"));
+
+    // create TupleStore
+    TupleStore store =
+        new TupleStore(false, true, true, 2, 5, 4, 2, namespace,
+            getTestResource("default.nt"));
+
+    // create example values
+    String[] values_eq = { "\"1\"^^<xsd:int>",
+      "\"2\"^^<xsd:int>",};
+    String[] values_neq = { "\"9\"^^<xsd:int>",
+      "\"7\"^^<xsd:int>",};
+
+    // store values in TupleStore, save integer-key in database
+    int[] ids = new int[2];
+    int i = 0;
+    for (String val : values_eq) {
+      ids[i++] = store.putObject(val);
+    }
+    int[] nids = new int[2];
+    i = 0;
+    for (String val : values_neq) {
+      nids[i++] = store.putObject(val);
+    }
+
+    // create FunctionalOperator
+    FunctionalOperator fop =
+        (FunctionalOperator)store.operatorRegistry
+        .checkAndRegister("de.dfki.lt.hfc.operators.IIncrement");
+
+    // create FEqual
+    FunctionalOperator feq =
+        (FunctionalOperator)store.operatorRegistry
+        .checkAndRegister("de.dfki.lt.hfc.operators.IEqual");
+
+    // do operation
+    ids[0] = fop.apply(ids);
+    assertEquals("1++ = 2", FunctionalOperator.TRUE, feq.apply(ids));
+    nids[0] = fop.apply(nids);
+    assertEquals("9++ != 7", FunctionalOperator.FALSE, feq.apply(nids));
+  }
+}
