e096e786d8848634696654c7625f84001abeb9da
Christophe Biwer <christophe.biwer@dfki.de>
2016-09-09 10:01:40 +0200

    added TestIsUri (works)

---
+++ src/test/java/de/dfki/lt/hfc/TestIsUri.java
@@ -0,0 +1,51 @@
+package de.dfki.lt.hfc;
+
+import static de.dfki.lt.hfc.Utils.*;
+import de.dfki.lt.hfc.types.XsdString;
+import de.dfki.lt.hfc.types.XsdBoolean;
+import de.dfki.lt.hfc.types.XsdInt;
+import de.dfki.lt.hfc.types.Uri;
+import static org.junit.Assert.*;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.junit.Test;
+
+/**
+ * @author Christophe Biwer, christophe.biwer@dfki.de
+ */
+public final class TestIsUri{
+
+  @Test
+  public void testcleanUpTuple() throws FileNotFoundException,
+      WrongFormatException, IOException, InterruptedException {
+
+    // load Namespace
+    Namespace namespace = new Namespace(getTestResource("default.ns"));
+
+    // create TupleStore
+    TupleStore store =
+        new TupleStore(false, true, true, 2, 5, 4, 2, namespace,
+            getTestResource("default.nt"));
+
+    // create FunctionalOperator
+    FunctionalOperator fop =
+        (FunctionalOperator)store.operatorRegistry
+        .checkAndRegister("de.dfki.lt.hfc.operators.IsUri");
+
+    // store values in TupleStore, save integer-key in database
+    int[] args = new int[4];
+
+    args[0] = store.putObject((new XsdString("0")).toString(true));
+    args[1] = store.putObject((new XsdInt(1)).toString(true));
+    args[2] = store.putObject((new XsdBoolean(false)).toString(true));
+    args[3] = store.putObject((new Uri("<rdf:type>")).toString(true));
+
+    // do operation
+    assertEquals("XsdString 0", FunctionalOperator.FALSE, fop.apply(new int[]{args[0]}));
+    assertEquals("XsdInt 1", FunctionalOperator.FALSE, fop.apply(new int[]{args[1]}));
+    assertEquals("XsdBoolean false", FunctionalOperator.FALSE, fop.apply(new int[]{args[2]}));
+    assertEquals("Uri <rdf:type>", FunctionalOperator.TRUE, fop.apply(new int[]{args[3]}));
+  }
+}
