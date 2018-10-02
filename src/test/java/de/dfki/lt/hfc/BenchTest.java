3ab163525aad0d46cc5ef888941b47db96425121
Christian Willms <christian.willms@dfki.de>
2018-10-02 12:24:33 +0200

    adapted Bench

--- src/test/java/de/dfki/lt/hfc/BenchTest.java
+++ src/test/java/de/dfki/lt/hfc/BenchTest.java
@@ -17,12 +17,12 @@ public class BenchTest {
   public void test() throws IOException, WrongFormatException {
     long start = System.currentTimeMillis();
     ForwardChainer fc = new ForwardChainer(Config.getInstance(TestingUtils.getTestResource("bench.yml")));
-    long mid = System.currentTimeMillis();
-    System.out.println("Reading took: " + (mid - start) + "ms");
+    //long mid = System.currentTimeMillis();
+    //System.out.println("Reading took: " + (mid - start) + "ms");
     fc.computeClosure();
-    long end = System.currentTimeMillis();
-    System.out.println("Reading and closure took: " + (end - start) + "ms");
-    System.out.println("closure took: " + (end - mid) + "ms");
+    //long end = System.currentTimeMillis();
+    //System.out.println("Reading and closure took: " + (end - start) + "ms");
+    //System.out.println("closure took: " + (end - mid) + "ms");
   }
 
   @Test
@@ -33,10 +33,8 @@ public class BenchTest {
     fc.computeClosure();
     String queryString = "Select * where ?s ?o ?p ";
     Query query = new Query(fc.tupleStore);
-    long start = System.currentTimeMillis();
     BindingTable bt = query.query(queryString);
-    long end = System.currentTimeMillis();
-    System.out.println("Query parsing took: " + (end - start));
+
   }
 
   @Test
@@ -45,10 +43,8 @@ public class BenchTest {
     // further PAL-specific tuples (special XSD DT)
     fc.uploadTuples(TestingUtils.getTestResource("ltworld.jena.nt"));
     fc.computeClosure();
-    long start = System.currentTimeMillis();
     boolean b = fc.tupleStore.ask(new String[]{"<ltw:obj_67783>", "<ltw:lt_technologicalApplication>", "<ltw:KB_788599_Individual_78>"});
-    long end = System.currentTimeMillis();
-    System.out.println("Query parsing took: " + (end - start));
+    //System.out.println("Query parsing took: " + (end - start));
     assertTrue(b);
   }
 
@@ -60,11 +56,9 @@ public class BenchTest {
     fc.computeClosure();
     String queryString = "Select ?s ?p where ?s <ltw:participatedIn> ?p ";
     Query query = new Query(fc.tupleStore);
-    long start = System.currentTimeMillis();
+
     BindingTable bt = query.query(queryString);
-    long end = System.currentTimeMillis();
-    System.out.println("Query parsing took: " + (end - start));
-    //System.out.println(bt.toString());
+
   }
 
   @Test
@@ -75,11 +69,8 @@ public class BenchTest {
     fc.computeClosure();
     String queryString = "Select ?s where ?s <rdf:type> <ltw:Active_Patent> ";
     Query query = new Query(fc.tupleStore);
-    long start = System.currentTimeMillis();
     BindingTable bt = query.query(queryString);
-    long end = System.currentTimeMillis();
-    System.out.println("Query parsing took: " + (end - start));
-    System.out.println(bt.toString());
+
   }
 
 }
