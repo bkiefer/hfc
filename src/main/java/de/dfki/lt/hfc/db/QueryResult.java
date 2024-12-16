0e321efee49de6acbe5d76b221dee6f91650c6c4
Bernd Kiefer <j_a_w_bone@gmx.de>
2024-12-16 13:59:41 +0100

    Add methods to save query results

--- src/main/java/de/dfki/lt/hfc/db/QueryResult.java
+++ src/main/java/de/dfki/lt/hfc/db/QueryResult.java
@@ -1,8 +1,28 @@
 package de.dfki.lt.hfc.db;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.dfki.lt.hfc.NamespaceManager;
+import de.dfki.lt.hfc.WrongFormatException;
+import de.dfki.lt.hfc.types.AnyType;
+import de.dfki.lt.hfc.types.XsdAnySimpleType;
+import de.dfki.lt.loot.digraph.DiGraph;
+import de.dfki.lt.loot.digraph.VertexListPropertyMap;
+import de.dfki.lt.loot.digraph.VertexPropertyMap;
+import de.dfki.lt.loot.digraph.io.GraphPrinterFactory;
 
 public class QueryResult {
+  private static final Logger logger = LoggerFactory.getLogger(QueryResult.class);
+
   public List<String> variables; // required
   public Table table; // required
 
@@ -21,5 +41,88 @@ public class QueryResult {
 
   public int getVariablesSize() {
     return variables.size();
+  }
+
+  public void saveToFile(Path filepath) throws IOException {
+    try (Writer fw = new FileWriter(filepath.toFile())) {
+      fw.append("# ");
+      for (String var: variables) {
+        fw.append(var).append(' ');
+      }
+      fw.append(System.lineSeparator());
+      for (List<String> row : table.rows) {
+        for (String val: row) {
+          fw.append(val).append(' ');
+        }
+        fw.append(".").append(System.lineSeparator());
+      }
+    }
+  }
+
+    /** Turn a URI string into an object.
+   *  A "normal" URI or blank node is turned into an RDF object, an XSD is
+   *  converted into a POD object.
+   *
+   * @param value
+   * @return
+   */
+  public static String getName(String value) {
+    AnyType o = null;
+    switch (value.charAt(0)) {
+      case '<': // URI
+        String[] nameNs = NamespaceManager.splitUriNsName(value);
+        return nameNs[0] + nameNs[1];
+      case '_': // blank node
+        return value;
+      case '"': // simple type
+        try {
+          o = XsdAnySimpleType.getXsdObject(value);
+          return o.toName();
+        } catch (WrongFormatException e) {
+          throw new RuntimeException(e);
+        }
+      default:
+        // Error: don't know what this is.
+        logger.warn("What's this: {}", value);
+        break;
+    }
+    return "ERR";
+  }
+
+  public void saveToGraph(Path filepath, boolean uniqueAtoms) throws IOException {
+    if (variables.size() != 3) {
+      logger.error("saveToGraph only handles triples");
+      return;
+    }
+    DiGraph<String> g = new DiGraph<>();
+    VertexPropertyMap<String> nodeName = new VertexListPropertyMap<String>(g);
+    Map<String, Integer> node2vertex = new HashMap<>();
+    // 'names' is the default map for the graph printer
+    g.register("names", nodeName);
+    for (List<String> row : table.rows) {
+      String sourcename = getName(row.get(0));
+      int source = -1;
+      if (node2vertex.containsKey(sourcename)) {
+        source = node2vertex.get(sourcename);
+      } else {
+        source = g.newVertex();
+        nodeName.put(source, sourcename);
+        node2vertex.put(sourcename, source);
+      }
+      String targetname = getName(row.get(2));
+      int target = -1;
+      if (uniqueAtoms && XsdAnySimpleType.isSimpleXsdType(targetname)) {
+        target = g.newVertex();
+        nodeName.put(target, targetname);
+      } else if (! node2vertex.containsKey(targetname)) {
+        target = g.newVertex();
+        nodeName.put(target, targetname);
+        node2vertex.put(targetname, target);
+      } else {
+        target = node2vertex.get(targetname);
+      }
+      g.newEdge(getName(row.get(1)), source, target);
+    }
+    GraphPrinterFactory.print(g, filepath);
   }
 }
