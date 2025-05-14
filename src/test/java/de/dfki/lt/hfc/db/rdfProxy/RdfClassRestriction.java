b9e90c75203bf302d7ff70564d902fe22502c4a2
Bernd Kiefer <bernd.kiefer@dfki.de>
2025-05-14 12:26:06 +0200

    Test: alterative RDF syntax does not break code

--- src/test/java/de/dfki/lt/hfc/db/rdfProxy/RdfClassRestriction.java
+++ src/test/java/de/dfki/lt/hfc/db/rdfProxy/RdfClassRestriction.java
@@ -14,11 +14,11 @@ public class RdfClassRestriction {
   private static final String RESOURCE_DIR = "src/test/data/";
 
   @Test
-  public void test() {
+  public void testRestrictions() {
     HfcDbHandler handler = new HfcDbHandler(RESOURCE_DIR + "restrictions.yml");
     RdfProxy proxy = new RdfProxy(handler);
     RdfClass test = proxy.getRdfClass("<restr:Test>");
-    assertEquals(4, test.getProperties().size());
+    assertEquals(8, test.getProperties().size());
     for (String prop : test.getProperties()) {
       if (prop.contains("Object")) {
         assertFalse(test.isDatatypeProperty(prop));
@@ -41,6 +41,37 @@ public class RdfClassRestriction {
     assertTrue((pt & RdfClass.DATATYPE_PROPERTY) != 0);
     assertTrue((pt & RdfClass.FUNCTIONAL_PROPERTY) != 0);
   }
+
+
+  @Test
+  public void testRestrictionsViaId() {
+    HfcDbHandler handler = new HfcDbHandler(RESOURCE_DIR + "restrictions.yml");
+    RdfProxy proxy = new RdfProxy(handler);
+    RdfClass test = proxy.getRdfClass("<restr:Test>");
+    assertEquals(8, test.getProperties().size());
+    for (String prop : test.getProperties()) {
+      if (prop.contains("Object")) {
+        assertFalse(test.isDatatypeProperty(prop));
+      } else {
+        assertTrue(test.isDatatypeProperty(prop));
+      }
+    }
+    Set<String> propRange = test.getPropertyRange("<restr:someObjectProperty2>");
+    assertEquals(1, propRange.size());
+    String pr = propRange.iterator().next();
+    RdfClass range = proxy.getRdfClass(pr);
+    assertEquals(range, proxy.getRdfClass("<restr:TestRange>"));
+    Rdf testInstance = test.getNewInstance("dom:");
+    RdfClass clazz = proxy.getMostSpecificClass(testInstance.getURI());
+    assertEquals(test, clazz);
+    int pt = test.getPropertyType("<restr:functionalObjectProperty2>");
+    assertTrue((pt & RdfClass.OBJECT_PROPERTY) != 0);
+    assertTrue((pt & RdfClass.FUNCTIONAL_PROPERTY) != 0);
+    pt = test.getPropertyType("<restr:functionalDataProperty2>");
+    assertTrue((pt & RdfClass.DATATYPE_PROPERTY) != 0);
+    assertTrue((pt & RdfClass.FUNCTIONAL_PROPERTY) != 0);
+  }
+
 
 
   /**
